2c80510d77af77f833ef260150ac61471489273e
MarianoIvasiuta26 <81333168+MarianoIvasiuta26@users.noreply.github.com>
2022-09-29 20:06:41 -0300

    creé el proyecto spring boot

---
+++ proyecto-consultorio/src/main/java/com/consultorio/proyectoconsultorio/ProyectoConsultorioApplication.java
@@ -0,0 +1,13 @@
+package com.consultorio.proyectoconsultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConsultorioApplication.class, args);
+	}
+
+}
